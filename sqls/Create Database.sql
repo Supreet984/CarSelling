effbac56373322fb0a87630273ae8f77067a8c0f
Supreet Ganguly <supreetganguly@gmail.com>
2022-07-20 10:37:54 +0300

    added sqls

---
+++ sqls/Create Database.sql
@@ -0,0 +1,12 @@
+-- Database: carselling_db
+
+-- DROP DATABASE IF EXISTS carselling_db;
+
+CREATE DATABASE carselling_db
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United Kingdom.1252'
+    LC_CTYPE = 'English_United Kingdom.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
